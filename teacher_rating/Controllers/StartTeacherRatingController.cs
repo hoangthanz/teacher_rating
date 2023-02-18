fc0f049b350674b958ad7e9cdf978b8777e1bc88
Hoàng Nguyễn Xuân <hoangxuan.ks6@gmail.com>
2023-02-18 16:51:13 +0700

    add data

--- teacher_rating/Controllers/StartTeacherRatingController.cs
+++ teacher_rating/Controllers/StartTeacherRatingController.cs
@@ -347,9 +347,305 @@ namespace teacher_rating.Controllers
                             AssessmentCriteriaGroupId = "46e5e5c4-3590-4575-a3e2-b5139a0998e4"
                         },
                     };
+                    
+                    var criteriasMinus3 = new List<AssessmentCriteria>()
+                    {
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "Vi phạm quy chế chuyên môn bị lập biên bản",
+                            Value = 20,
+                            DeductScore = 20,
+                            Unit = "Lần",
+                            IsDeduct = true,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "Ra đề, phản biện đề HSG, đề kiểm tra chung có lỗi văn bản",
+                            Value = 2,
+                            DeductScore = 2,
+                            Unit = "Lỗi",
+                            IsDeduct = true,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "Ra đề, phản biện đề HSG, đề kiểm tra chung có lỗi kiến thức",
+                            Value = 10,
+                            DeductScore = 10,
+                            Unit = "Lỗi",
+                            IsDeduct = true,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "Coi thi HSG, Kiểm tra chung không nghiêm túc",
+                            Value = 10,
+                            DeductScore = 10,
+                            Unit = "Lỗi",
+                            IsDeduct = true,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "Chấm HSG, kiểm tra có sai sót",
+                            Value = 10,
+                            DeductScore = 10,
+                            Unit = "Lỗi",
+                            IsDeduct = true,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "Kiểm tra hồ sơ, thanh tra chuyên môn không sát thực, bao che, chủ quan cảm tính",
+                            Value = 10,
+                            DeductScore = 10,
+                            Unit = "Lỗi",
+                            IsDeduct = true,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "Không tham gia hoạt động của tổ chuyên môn",
+                            Value = 10,
+                            DeductScore = 10,
+                            Unit = "HĐ",
+                            IsDeduct = true,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "Quản lý giờ học thiếu trách nhiệm, bỏ dạy để học sinh ngồi chơi",
+                            Value = 10,
+                            DeductScore = 10,
+                            Unit = "Tiết",
+                            IsDeduct = true,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                    };
+                    
+                     var criteriasPlus3 = new List<AssessmentCriteria>()
+                    {
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "Ra đề HSG, Kiểm tra chung",
+                            Value = 20,
+                            DeductScore = 20,
+                            Unit = "Đề",
+                            IsDeduct = false,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "Phản biện đề HSG, Kiểm tra chung",
+                            Value = 10,
+                            DeductScore = 10,
+                            Unit = "Đề",
+                            IsDeduct = false,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "Chấm kiểm tra chung trắc nghiệm",
+                            Value = 20,
+                            DeductScore = 20,
+                            Unit = "Khối",
+                            IsDeduct = false,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "Kiểm tra chung tự luận",
+                            Value = 10,
+                            DeductScore = 10,
+                            Unit = "Lần",
+                            IsDeduct = false,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "Chấm HSG",
+                            Value = 20,
+                            DeductScore = 20,
+                            Unit = "Lần",
+                            IsDeduct = false,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "Kiểm tra hồ sơ",
+                            Value = 10,
+                            DeductScore = 10,
+                            Unit = "Lần",
+                            IsDeduct = false,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "Dạy thay do tổ trưởng phân công",
+                            Value = 3,
+                            DeductScore = 3,
+                            Unit = "Tiết",
+                            IsDeduct = false,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "Ôn HSG được tổ trưởng xác nhận",
+                            Value = 10,
+                            DeductScore = 10,
+                            Unit = "Buổi",
+                            IsDeduct = false,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "HSG Quốc gia",
+                            Value = 60,
+                            DeductScore = 60,
+                            Unit = "",
+                            IsDeduct = false,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "HSG thành phố giải nhất",
+                            Value = 30,
+                            DeductScore = 30,
+                            Unit = "Giải",
+                            IsDeduct = false,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "HSG thành phố giải nhì",
+                            Value = 25,
+                            DeductScore = 25,
+                            Unit = "Giải",
+                            IsDeduct = false,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "HSG thành phố giải ba",
+                            Value = 20,
+                            DeductScore = 20,
+                            Unit = "Giải",
+                            IsDeduct = false,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "HSG thành phố khuyến khích",
+                            Value = 15,
+                            DeductScore = 15,
+                            Unit = "Giải",
+                            IsDeduct = false,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "GVG trường",
+                            Value = 20,
+                            DeductScore = 20,
+                            Unit = "Giải",
+                            IsDeduct = false,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "GVG cụm",
+                            Value = 25,
+                            DeductScore = 25,
+                            Unit = "Giải",
+                            IsDeduct = false,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "GVG TP",
+                            Value = 30,
+                            DeductScore = 30,
+                            Unit = "Giải",
+                            IsDeduct = false,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "SK A trường",
+                            Value = 10,
+                            DeductScore = 10,
+                            Unit = "Giải",
+                            IsDeduct = false,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                        new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name =
+                                "SK A TP",
+                            Value = 20,
+                            DeductScore = 20,
+                            Unit = "Giải",
+                            IsDeduct = false,
+                            AssessmentCriteriaGroupId = "e74719f0-1477-4e39-bc07-618b5f3cc469"
+                        },
+                    };
                     allCriteriaGroups.AddRange(criteriasMinus1);
                     allCriteriaGroups.AddRange(criteriasMinus2);
                     allCriteriaGroups.AddRange(criteriasPlus2);
+                    allCriteriaGroups.AddRange(criteriasMinus3);
+                    allCriteriaGroups.AddRange(criteriasPlus3);
                     await _assessmentCriteriaRepository.AddAssessmentCriterList(allCriteriaGroups);
                 }
 
