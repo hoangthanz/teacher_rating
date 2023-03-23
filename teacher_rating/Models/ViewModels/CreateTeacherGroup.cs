b8a9b1f4f94ffb99df64d54eb291065f5f84eed6
DESKTOP-5SP8TKQ\HoangThan <xuanhoang.ks6@gmail.com>
2023-03-23 15:10:23 +0700

    fix bug

---
+++ teacher_rating/Models/ViewModels/CreateTeacherGroup.cs
@@ -0,0 +1,19 @@
+﻿using teacher_rating.Common.Const;
+
+namespace teacher_rating.Models.ViewModels;
+
+public class CreateTeacherGroup
+{
+    public string Name { get; set; }
+    public List<string>? TeacherIds { get; set; }
+    public double Period1Score { get; set; } = 0;
+
+    public double Period2Score { get; set; } = 0;
+
+    public double YearScore { get; set; } = 0;
+
+    public string SchoolId { get; set; } = DefaultConfigs.DefaultSchoolId;
+
+
+    public double TotalMember { get; set; } = 0;
+}
